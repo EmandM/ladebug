1ce2740b9ed24539795f7165881c8de6d0b6ab46
Libby713 <e_stevenson@xtra.co.nz>
2017-06-12 00:06:11 +1200

    Added memory display block, function for content not working

--- src/app/components/home/home.component.js
+++ src/app/components/home/home.component.js
@@ -48,6 +48,36 @@ class homeController {
   toggleBreakpoint(lineNumber) {
     this.breakpoints[lineNumber] = !this.breakpoints[lineNumber];
   }
+
+  memoryVarsToString() {
+    // for each heap item not a function
+    //    get it's name from the ordered global variables
+    //    for each item in the heap
+    //      get it's name and value
+    //      concatenate to memoryString
+    //    add separating ; to memoryString
+    // add separating \n to memoryString
+
+    var memoryString = "";
+    var currentHeap = this.codeTrace[this.currentTraceIndex].heap;
+    var currentGlobalVars = this.codeTrace[this.currentTraceIndex].ordered_globals;
+    var currentVar;
+
+    //problem starts here - currentHeap has no length as it is an object
+    console.log("heap", currentHeap);
+
+    for (var i = 0; i < currentHeap.length; i++) {
+      currentVar = currentHeap[i];
+      console.log("what the heap object is: " + currentVar[0]);
+      if (currentVar[0] != "FUNCTION") {
+        for (var j = 1; j < currentVar.length; j++) {
+          memoryString.concat(currentVar[j].toString());
+        }
+      }
+    }
+
+    return memoryString;
+  }
 }
 
 homeController.$inject = ['$mdDialog'];
