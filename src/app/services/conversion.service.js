9695e5227ccf6bc9cc0723ac7e86f7626387be29
Libby713 <e_stevenson@xtra.co.nz>
2017-07-19 19:41:19 +1200

    Conversion service successfully prints to console

--- src/app/services/conversion.service.js
+++ src/app/services/conversion.service.js
@@ -6,9 +6,9 @@ class ConversionService {
   }
 
   postRequest(pythonString) {
-    //restangularr wowee
-    console.log('ye', pythonString);
+    console.log('yo');
   
+    //restangularr wowee
     //var code = Restangular.all('sandbox');
   }
 
@@ -18,15 +18,3 @@ ConversionService.$inject = ['Restangular'];
 
 angular.module('debugapp')
   .service('ConversionService', ConversionService);
-
-/*
-  1) send python string to service
-  2) service makes POST request to server with python string for below work
-  3) - server converts python to json
-     - server returns json to service
-  4) service makes unique id for json created
-  5) sandbox page changes to debug page, sending json id to debug page through
-     param (app.routing.js)
-  6) debug page sends json id to service asking for corresponding json
-  7) service returns corresponding json to debug page
-*/
