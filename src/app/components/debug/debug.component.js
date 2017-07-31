9dc275a881677b04a9a133eda60acb775140124a
Libby713 <e_stevenson@xtra.co.nz>
2017-08-01 01:13:24 +1200

    Created basic modals for correct and incorrect answers

--- src/app/components/debug/debug.component.js
+++ src/app/components/debug/debug.component.js
@@ -1,16 +1,20 @@
 import angular from 'angular';
 import findIndex from 'lodash/findIndex';
 import drop from 'lodash/drop';
+//import moment from 
 import TraceToCallStack from '../../helpers/trace-to-call-stack.helper';
 import template from './debug.template.html';
 import './debug.scss';
 
 class debugController {
-  constructor(exerciseService) {
+  constructor(exerciseService, $mdDialog) {
     this.exerciseService = exerciseService;
+    this.$mdDialog = $mdDialog;
 
     // Object for breakpoints => faster lookup than array.
     this.breakpoints = {};
+    //this.startTime = moment();
+    this.incorrectGuesses = 0;
   }
 
   $onInit() {
@@ -18,6 +22,7 @@ class debugController {
       .then((response) => {
         this.codeString = response.debugInfo.code;
         this.codeTrace = response.debugInfo.trace;
+        this.bug_line = response.bug_line;
         if (response.name) {
           this.existingExercise = true;
           this.pageName = response.name;
@@ -74,7 +79,7 @@ class debugController {
     }
   }
 
-  submit() {
+  submit($event) {
     if(!this.errorLine) {
       return;
     }
@@ -82,11 +87,45 @@ class debugController {
       return;
     }
 
-    this.submitted = true;
+    //this.endTime = moment();
+    console.log("entered: " + this.errorLine + ", actual: " + this.bug_line);
+
+    if(this.errorLine == this.bug_line) {
+      this.submitted = true;
+      //timeTaken = this.endTime - this.startTime;
+      var timeTaken = '11 seconds';
+      var statistics = 'Time taken: ' + timeTaken + '; ' + 'Incorrect guesses: ' + this.incorrectGuesses.toString();
+      //correct line modal
+      this.$mdDialog.show(
+      this.$mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title('Good Work!')
+        .textContent('You entered the correct line, ' + this.errorLine + '! ' + statistics)
+        .ariaLabel('Correct Line Alert Dialog')
+        .ok('OK')
+        .targetEvent($event)
+      );
+      
+      return;
+    }
+
+    this.incorrectGuesses += 1;
+    //incorrect line modal
+    this.$mdDialog.show(
+      this.$mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title('Incorrect Line')
+        .textContent('Please try again.')
+        .ariaLabel('Incorrect Line Alert Dialog')
+        .ok('OK')
+        .targetEvent($event)
+    );
   }
 }
 
-debugController.$inject = ['ExerciseService'];
+debugController.$inject = ['ExerciseService', '$mdDialog'];
 
 angular.module('debugapp')
   .component('debug', {
