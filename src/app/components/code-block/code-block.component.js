69827b8d5e8a6919f1268019a5c8c41260e05a14
EmandM <loopylemon8@gmail.com>
2017-09-07 11:44:42 +1200

    Update code-block to correctly format block comments

--- src/app/components/code-block/code-block.component.js
+++ src/app/components/code-block/code-block.component.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 import split from 'lodash/split';
+import trim from 'lodash/trim';
+import startsWith from 'lodash/startsWith';
+import forEach from 'lodash/forEach';
 
 import template from './code-block.template.html';
 import './code-block.scss';
@@ -12,12 +15,53 @@ class codeBlockController {
     if (changesObj.codeString) {
       // Split the code by newline characters to have reference to each line.
       this.codeByLines = split(this.codeString, '\n');
+      this.calculateCommentBlockLines();
     }
+  }
+
+  // Function to find comment blocks and save their line numbers
+  calculateCommentBlockLines() {
+    let inCommentBlock = false;
+    // Triple quotes are a marker for comment blocks in python
+    const commentBlockMarker = '\'\'\'';
+    this.commentAndEmptyLines = {};
+    this.blockComments = {};
+    forEach(this.codeByLines, (currentLine, lineIndex) => {
+      if (inCommentBlock) {
+        this.commentAndEmptyLines[lineIndex] = true;
+        this.blockComments[lineIndex] = true;
+        return;
+      }
+      const trimmedLine = trim(currentLine);
+      if (!trimmedLine || startsWith(trimmedLine, '#')) {
+        this.commentAndEmptyLines[lineIndex] = true;
+        return;
+      }
+      if (trimmedLine === commentBlockMarker) {
+        if (!inCommentBlock) {
+          this.commentAndEmptyLines[lineIndex] = true;
+          this.blockComments[lineIndex] = true;
+        }
+        inCommentBlock = !inCommentBlock;
+      }
+    });
+  }
+
+  isWhitespaceOrComment(lineNum) {
+    return this.commentAndEmptyLines[lineNum - 1];
   }
 
   toggleIcon(lineNum, iconType) {
     if (this.isEditing) {
       return;
+    }
+
+    if (iconType === 'breakpoint') {
+      // if line is empty or is a comment, try put a breakpoint on the next line
+      if (this.isWhitespaceOrComment(lineNum)) {
+        this.toggleIcon(lineNum + 1, iconType);
+        return;
+      }
     }
     this.iconAction({ line: lineNum, icon: iconType });
   }
