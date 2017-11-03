b737d6e889d823b40dd5d8d9d073b2eed094be6a
EmandM <loopylemon8@gmail.com>
2017-11-03 17:35:09 +1300

    Fix issue where sign in button shows during page load when user is signed in

--- src/app/components/page-header/page-header.component.js
+++ src/app/components/page-header/page-header.component.js
@@ -4,48 +4,55 @@ import template from './page-header.template.html';
 import './page-header.scss';
 
 class headerController {
-  constructor(authService, $scope) {
+  constructor(authService, $scope, $timeout) {
     this.authService = authService;
     this.$scope = $scope;
+    this.$timeout = $timeout;
 
-    this.authService.loadApi()
-      .catch(() => {
+    this.authService.checkSignedIn()
+      .then((isSignedIn) => {
+        this.loadAuth(isSignedIn);
+        this.authLoaded = true;
+      })
+      .catch((error) => {
+        console.log('error: ', error);
         this.noGapi = true;
       });
     this.authServiceKey = 'page-header';
     this.authService.addOnSignIn(this.authServiceKey, this.authChange.bind(this));
     this.authService.renderSignInButton('signInButton');
   }
+
   $onDestroy() {
     this.authService.removeOnSignIn(this.authServiceKey);
   }
 
   applyScope() {
-    this.$scope.$apply();
+    this.$timeout(this.$scope.$apply(), 0);
   }
 
   authChange(isSignedIn) {
-    if (!isSignedIn) {
+    this.loadAuth(isSignedIn);
+    this.applyScope();
+  }
+
+  loadAuth(isSignedIn) {
+    this.isSignedIn = isSignedIn;
+    if (!this.isSignedIn) {
       return;
     }
     const userInfo = this.authService.getUserInfo();
     this.userName = userInfo.getName();
     this.userImage = userInfo.getImageUrl();
     this.userEmail = userInfo.getEmail();
-    this.userLoaded = true;
-    this.applyScope();
   }
 
   signout() {
-    this.authService.signOut()
-      .then(() => {
-        this.userLoaded = false;
-        this.applyScope();
-      });
+    this.authService.signOut();
   }
 }
 
-headerController.$inject = ['AuthService', '$scope'];
+headerController.$inject = ['AuthService', '$scope', '$timeout'];
 
 angular.module('debugapp')
   .component('pageHeader', {
