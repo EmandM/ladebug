eda21af9ca3a9bfdf9f28292df83963f72d5dbd5
EmandM <loopylemon8@gmail.com>
2017-12-03 01:19:56 +1300

    Display nested variables with the correct height

--- src/app/components/variable/variable.component.js
+++ src/app/components/variable/variable.component.js
@@ -6,7 +6,10 @@ import './variable.scss';
 class variableController {
   // constructor() { }
 
-  $onChanges() {
+  $onChanges(changesObj) {
+    if (!changesObj.variable || !this.variable) {
+      return;
+    }
     this.isString = (this.variable.type === 'string');
     this.isPrimitive = this.variable.isPrimitive;
     this.hasName = (this.variable.name !== undefined);
